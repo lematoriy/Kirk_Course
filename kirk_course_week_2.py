0dd4bf12186f35f920e0e5d6decbde5741b2f383
lematoriy <lematoriy@gmail.com>
2017-01-06 10:54:45 +1300

    completed

--- kirk_course_week_2.py
+++ kirk_course_week_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import re
+
 # ip input - check if at least 3 octets are specified
 while True:
     ip_address=raw_input("Enter the ip address: ")
@@ -41,6 +43,22 @@ print '\n\n'
 
 
 entry1 = "*  1.0.192.0/18   157.130.10.233        0 701 38040 9737 i"
-entry2 = "*  1.1.1.0/24      157.130.10.233         0 701 1299 15169 i"
-entry3 = "*  1.1.42.0/24     157.130.10.233        0 701 9505 17408 2.1465 i"
-entry4 = "*  1.0.192.0/19   157.130.10.233        0 701 6762 6762 6762 6762 38040 9737 i"
+entry1.split()
+
+print 'Entry line is: %s' % (entry1)
+print 'ip prefix: %s' % (entry1.split()[1])
+print 'AS Path: %s' % (entry1.split()[3:])
+print '\n\n'
+
+cisco_ios = "Cisco IOS Software, C880 Software (C880DATA-UNIVERSALK9-M), Version 15.0(1)M4, RELEASE SOFTWARE (fc1)"
+
+# using rex, .*?)\) - any number of characters before ) in non-greedy manner '?'
+# - otherwise capture till last )
+software=re.search('Software *\((.*?)\)',cisco_ios).group(1)
+ios=re.search('Version *(.*),',cisco_ios).group(1)
+
+print '\n\n'
+print 'IOS details:'
+print '%25s %25s' % ('Software','Version')
+print '%25s %25s' % (software,ios)
+print '\n\n'
