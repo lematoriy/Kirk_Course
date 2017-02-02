f8417b9dfefd048e334446f073bb008b94e7bf12
lematoriy <lematoriy@gmail.com>
2017-02-02 23:22:24 +1300

    snmp 2017-02-02-23:21

--- networking_test_snmp.py
+++ networking_test_snmp.py
@@ -6,9 +6,16 @@ snmp-server user mishav2c V2CALLRO v2c
 snmp-server group V3ALLRO v3 priv read VIEWALLRO
 snmp-server group V2CALLRO v2c read VIEWALL
 snmp-server view VIEWALL ccitt included
-snmp-server community mishacommunity view VIEWALLRO RO
-snmp-server user mishav3 V3ALLRO v3 auth sha mishamisha priv aes 128 mishamisha
+snmp-server view VIEWALLRO iso.* included
+snmp-server community mishacommunity RO
 R1#
+snmp-server user mishav3 V3ALLRO v3 auth sha mishamisha priv aes 128 mishamisha
+'''
+'''
+ubuntu@ubuntu-VirtualBox:~$ snmpget -v 3 -u mishav3 -l authpriv -a SHA -A "mishamisha" -x AES -X "mishamisha" 192.168.100.1:161 .1.3.6.1.2.1.4.24.3.0
+IP-FORWARD-MIB::ipCidrRouteNumber.0 = Gauge32: 2
+ubuntu@ubuntu-VirtualBox:~$ snmpget -v 2c -c mishacommunity 192.168.100.1 system.sysUpTime.0
+DISMAN-EVENT-MIB::sysUpTimeInstance = Timeticks: (664610) 1:50:46.10
 '''
 
 import pprint
@@ -28,14 +35,14 @@ cmdGen = cmdgen.CommandGenerator()
 #                                                                               cmdgen.UdpTransportTarget((ip, 161)),
 #                                                                               '1.3.6.1.2.1.14.10.1.3')
 
-#pprint.pprint(cmdGen.nextCmd(cmdgen.CommunityData(comm),cmdgen.UdpTransportTarget((ip, 161)),'1.3.6.1.2.1.14.10.1.3'))
+#pprint.pprint(cmdGen.nextCmd(cmdgen.CommunityData(comm),cmdgen.UdpTransportTarget((ip, 161)),'1.3.6.1.2.1.1.3'))
 #pprint.pprint(cmdGen.nextCmd(cmdgen.CommunityData(comm),cmdgen.UdpTransportTarget((ip, 161)),'1.3.6.1.2.1.4.21.1.1'))
 
 errorIndication, errorStatus, errorIndex, varBindNbrTable = cmdGen.nextCmd(cmdgen.CommunityData(comm),
                                                                                cmdgen.UdpTransportTarget((ip, 161)),
-                                                                               '1.3.6.1.2.1.1')
+                                                                               '1.3.6.1.2.1.1.3')
 
-pprint.pprint(errorIndication)
-pprint.pprint(errorStatus)
+#pprint.pprint(errorIndication)
+#pprint.pprint(errorStatus)
 print '\n'
 pprint.pprint(varBindNbrTable)
